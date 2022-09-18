19a38831b0fbf8747216cdc7e4ae68363feb9c2e
Sandip <sandip@ssdd.dev>
2022-09-18 20:24:08 +0530

    init

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { ReactComponent as IconFacebook } from './assets/icons/facebook.svg';
+import { ReactComponent as IconTwitter } from './assets/icons/twitter.svg';
+import { ReactComponent as IconGithub } from './assets/icons/github.svg';
+import "./App.css";
+
+class App extends React.Component {
+  render = () => {
+    return (
+      <div className="card">
+        <div className="header">
+          <div className="logo">
+            <a href=".">LOGO</a>
+          </div>
+          <div className="social">
+            <a href="https://github.com/ssddcodes/slang" title="GitHub" target="_blank" rel="noopener noreferrer">
+              <IconGithub className="icon" />
+            </a>
+          </div>
+        </div>
+        <div className="content">
+          <div className="title-holder">
+            <h1>Get ready for the change.</h1>
+            <p>Website coming soon. Please check back to know more. Shoot us an email if you're curious.</p>
+          </div>
+          <a href="mailto:support@slang.live">
+            <div className="cta">Send us an email</div>
+          </a>
+        </div>
+        <div className="footer">
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
